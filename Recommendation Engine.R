0d630342b61f4b8cb62fd155f463dea8ea86ce74
monkeydluffy22 <tejascpatil22@gmail.com>
2018-11-08 22:29:04 +0530

    Add files via upload

---
+++ Recommendation Engine.R
@@ -0,0 +1,33 @@
+# Recommendation system 
+
+# Applications - Movies(Netflix, Hulu, Prime, Hotstar) Events, Books, Ecommerce purchase, Jobs, Travel and Real Estate, News, Youtube, Spotify
+
+
+
+
+## Movie Recommendation Engine - 
+
+# Variables that can come in handy 
+
+# genre, tag, year of release, rating, actor, director, similar viewers, comments, tags, tweets, social media reviews 
+# time of the year, time of the day, location
+# age, gender, mood(?), previous choices, ease with which person gives 5
+
+
+## Methods of Recommendation Engine
+## --------------------------------------------------------------------------------------------------------------------
+# Content Based
+# Collaborative filtering(Memory, Model)
+# Deep Learning
+## --------------------------------------------------------------------------------------------------------------------
+
+## Content based is by clustering similar movies together.
+
+
+
+# Content based is not a bad idea, the idea of clustering similar movies together itself is a daunting task
+
+# A drawback of Collaborative filtering is that if some movies are highly underrated, there is a high probability that it will not show up 
+# in the results produced by a recommendation engine 
+
+# What Movie attributes can be used to find similar movie 
